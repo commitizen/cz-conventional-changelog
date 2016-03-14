b311b4849095e273c5f65607d00ede99efc87de2
Billy McIntosh <billymcintosh@gmail.com>
2016-03-14 14:01:30 +0100

    add jira ticket

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var wrap = require('word-wrap');
 
 // This can be any kind of SystemJS compatible module.
-// We use Commonjs here, but ES6 or AMD would do just 
+// We use Commonjs here, but ES6 or AMD would do just
 // fine.
 module.exports = {
 
@@ -11,26 +11,30 @@ module.exports = {
   // be executed. We pass you cz, which currently
   // is just an instance of inquirer.js. Using
   // this you can ask questions and get answers.
-  // 
+  //
   // The commit callback should be executed when
   // you're ready to send back a commit template
-  // to git. 
-  // 
+  // to git.
+  //
   // By default, we'll de-indent your commit
   // template and will keep empty lines.
   prompter: function(cz, commit) {
 
     console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
-    
+
     // Let's ask some questions of the user
-    // so that we can populate our commit 
-    // template. 
+    // so that we can populate our commit
+    // template.
     //
     // See inquirer.js docs for specifics.
     // You can also opt to use another input
     // collection library if you prefer.
     cz.prompt([
       {
+        type: 'input',
+        name: 'jira',
+        message: 'JIRA Ticket ID:\n'
+      }, {
         type: 'list',
         name: 'type',
         message: 'Select the type of change that you\'re committing:',
@@ -78,7 +82,7 @@ module.exports = {
         message: 'List any breaking changes or issues closed by this change:\n'
       }
     ], function(answers) {
-      
+
       var maxLineWidth = 100;
 
       var wrapOptions = {
@@ -93,7 +97,7 @@ module.exports = {
       scope = scope ? '(' + answers.scope.trim() + ')' : '';
 
       // Hard limit this line
-      var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+      var head = answers.jira + ' ' + (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
 
       // Wrap these lines at 100 characters
       var body = wrap(answers.body, wrapOptions);
