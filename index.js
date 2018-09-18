65d6a9f935318398ea120da406d76b78498ca632
Sean McCollum <smccollum@blurb.com>
2018-09-18 07:25:19 -0700

    feat: add default values to options (#69)
    For my own workflow, I made an adapter to allow passing default values in gleaned from the branch name, but needed to somehow get those values into this adapter, and I thought this was a good way to achieve this goal.
    
    With these changes, my custom adapter can be like this:
    ```
    process.env.CZ_TYPE = "fix"
    module.exports = require("cz-conventional-changelog")
    ```
    
    Instead of having to copy everything.

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ var engine = require('./engine');
 var conventionalCommitTypes = require('conventional-commit-types');
 
 module.exports = engine({
-  types: conventionalCommitTypes.types
+  types: conventionalCommitTypes.types,
+  defaultType: process.env.CZ_TYPE,
+  defaultScope: process.env.CZ_SCOPE,
+  defaultSubject: process.env.CZ_SUBJECT,
+  defaultBody: process.env.CZ_BODY,
+  defaultIssues: process.env.CZ_ISSUES
 });
