acb753417b3a4faa585de7e6e91993dfe122d07c
rhinel <rhinelkc@hotmail.com>
2019-03-17 19:26:16 +0800

    功能: 全量修改

--- engine.js
+++ engine.js
@@ -11,9 +11,8 @@ var filter = function(array) {
   });
 };
 
-// This can be any kind of SystemJS compatible module.
-// We use Commonjs here, but ES6 or AMD would do just
-// fine.
+// 这可以是任何类型的SystemJS兼容模块。
+// 我们在这里使用Commonjs，但ES6或AMD也兼容。
 module.exports = function (options) {
 
   var types = options.types;
@@ -27,70 +26,59 @@ module.exports = function (options) {
   });
 
   return {
-    // When a user runs `git cz`, prompter will
-    // be executed. We pass you cz, which currently
-    // is just an instance of inquirer.js. Using
-    // this you can ask questions and get answers.
-    //
-    // The commit callback should be executed when
-    // you're ready to send back a commit template
-    // to git.
-    //
-    // By default, we'll de-indent your commit
-    // template and will keep empty lines.
+    // 当用户运行`git cz`时，将执行提示符。我们传递给你cz，
+    // 使用此功能，您可以提出问题并获得答案。
+    // 当您准备好将提交模板发送回git时，应该执行提交回调。
+    // 默认情况下，我们将取消缩进您的提交模板并保留空行。
     prompter: function(cz, commit) {
-      console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+      console.log('\n第1行将被裁剪为100个字符，所有其他行将在100个字符后换行。\n');
 
-      // Let's ask some questions of the user
-      // so that we can populate our commit
-      // template.
-      //
-      // See inquirer.js docs for specifics.
-      // You can also opt to use another input
-      // collection library if you prefer.
+
+      // 让我们问一下用户的一些问题，这样我们就可以填充我们的提交模板。
+      // 您也可以选择使用其他输入，如果你愿意，可以收集库。
       cz.prompt([
         {
           type: 'list',
           name: 'type',
-          message: 'Select the type of change that you\'re committing:',
+          message: '选择您要提交的更改类型：',
           choices: choices,
           default: options.defaultType
         }, {
           type: 'input',
           name: 'scope',
-          message: 'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n',
+          message: '此代码提交的范围是什么（例如组件或文件名）？（按enter键跳过）\n',
           default: options.defaultScope
         }, {
           type: 'input',
           name: 'subject',
-          message: 'Write a short, imperative tense description of the change:\n',
+          message: '写一个简短的，命令式的代码提交描述：\n',
           default: options.defaultSubject
         }, {
           type: 'input',
           name: 'body',
-          message: 'Provide a longer description of the change: (press enter to skip)\n',
+          message: '请提供更长的更改提交描述：(按enter键跳过）\n',
           default: options.defaultBody
         }, {
           type: 'confirm',
           name: 'isBreaking',
-          message: 'Are there any breaking changes?',
+          message: '有没有不兼容性的更新？',
           default: false
         }, {
           type: 'input',
           name: 'breaking',
-          message: 'Describe the breaking changes:\n',
+          message: '描述不兼容更新：\n',
           when: function(answers) {
             return answers.isBreaking;
           }
         }, {
           type: 'confirm',
           name: 'isIssueAffected',
-          message: 'Does this change affect any open issues?',
+          message: '本次代码提交是否与任何问题关联？',
           default: options.defaultIssues ? true : false
         }, {
           type: 'input',
           name: 'issues',
-          message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
+          message: '添加问题编号参考 (例如："fix #123", "re #123".):\n',
           when: function(answers) {
             return answers.isIssueAffected;
           },
@@ -107,19 +95,19 @@ module.exports = function (options) {
           width: maxLineWidth
         };
 
-        // parentheses are only needed when a scope is present
+        // 仅在存在范围时才需要括号
         var scope = answers.scope.trim();
         scope = scope ? '(' + answers.scope.trim() + ')' : '';
 
-        // Hard limit this line
+        // 硬限制这条线
         var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
 
-        // Wrap these lines at 100 characters
+        // 将这些行换行为100个字符
         var body = wrap(answers.body, wrapOptions);
 
-        // Apply breaking change prefix, removing it if already present
+        // 应用中断更改前缀，如果已存在则将其删除
         var breaking = answers.breaking ? answers.breaking.trim() : '';
-        breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
+        breaking = breaking ? '不兼容更新: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
         breaking = wrap(breaking, wrapOptions);
 
         var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "format cjs";
 
 var engine = require('./engine');
-var conventionalCommitTypes = require('conventional-commit-types');
+var conventionalCommitTypes = require('./conventional-commit-types');
 
 module.exports = engine({
   types: conventionalCommitTypes.types,
